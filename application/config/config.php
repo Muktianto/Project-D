d2d2a9ed562c323ee399362d5b3e34c6c4ccd3d0
Dwi Muktianto <dwi.muktianto@gmail.com>
2020-03-08 00:15:08 +0700

    add

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:88/Project-D/';
-// $config['base_url'] = 'http://localhost/project-d/';
+// $config['base_url'] = 'http://localhost:88/Project-D/';
+$config['base_url'] = 'http://localhost/Project-D/';
 
 /*
 |--------------------------------------------------------------------------
